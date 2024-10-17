914d08d9d2b405b5f7a9887cecc426c867d980e6
kishansoftware <kumarprince955961@gmail.com>
2024-10-17 21:37:50 +0530

    arraysubtractionmatrix

--- arraymatrixsubtraction.c
+++ arraymatrixsubtraction.c
@@ -1,24 +1,21 @@
 #include<stdio.h>
-void addmatrix(int c[2][2],int a[2][2],int b[2][2],int m,int n)
+void submatrix(int c[2][2],int a[2][2],int b[2][2],int m,int n)
 {
 	int i,j;
-	
-	for(i=0;i<=m-1;i++)
-	{
+for(i=0;i<=m-1;i++)
+{
 	for(j=0;j<=n-1;j++)
 	{
 		c[i][j]=a[i][j]-b[i][j];
 	}
 	printf("\n");
-	
 }
-	
 }
 void printmatrix(int a[2][2],int m,int n)
 {
-	int i,j;
-		for(i=0;i<=m-1;i++)
-	{
+int i,j;
+for(i=0;i<=m-1;i++)
+{
 	for(j=0;j<=n-1;j++)
 	{
 		printf("%4d",a[i][j]);
@@ -29,14 +26,16 @@ printf("\n");
 }
 int main()
 {
-	int a[2][2]={{1,2},{3,4}};
-	int b[2][2]={{5,6},{7,8}};
-	int c[2][2];
-	int m=2,n=2;
-	int j,i;
+int a[2][2]={{5,8},{8,9}};
+int b[2][2]={{1,3},{2,1}};
+int c[2][2];
+int m=2,n=2;
+int i,j;
 printmatrix(a,2,2);
 printmatrix(b,2,2);
-addmatrix(c,a,b,2,2);
+submatrix(c,a,b,2,2);
 printmatrix(c,2,2);
 return 0;
+
 }
+
